a4a4c9a17bf1e68a5fe9e8afbbd860d0b92413aa
Yuval Dekel <thedekel@gmail.com>
2013-09-16 11:21:52 -0400

    basic starting point for demo

---
+++ 00_basic_bottle_app.py
@@ -0,0 +1,19 @@
+from bottle import route, run
+
+# simple list of books we can offer
+books = ['pizza', 'burgers', 'pancakes']
+pizza_counter = 0
+
+
+@route('/')
+@route('/books')
+def index():
+  """The index page should show all the books, also accessible through /books"""
+  return "This is where I show all the books I have!"
+
+@route('/books/<book_id>')
+def add_item(book_id):
+  """this page should show information about a single book"""
+  return "You requested the page for the book: %s" % book_id
+
+run(host='localhost', port=8080)
