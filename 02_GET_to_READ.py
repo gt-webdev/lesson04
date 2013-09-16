a6709ed464d6bd6f45415c0d8eb149a479f5f4e4
Yuval Dekel <thedekel@gmail.com>
2013-09-16 11:46:59 -0400

    added better /books and /books/id

---
+++ 02_GET_to_READ.py
@@ -0,0 +1,41 @@
+from bottle import route, run, request, redirect, abort
+
+# simple list of books we can offer
+books = ['Lord of the Webdev', 
+  'HitchHiker\'s Guide to Webdev', 
+  'Webdev: the Good Parts']
+pizza_counter = 0
+
+
+@route('/')
+@route('/books')
+def index():
+  """The index page should show all the books, also accessible through /books"""
+  books_list = '<ul>'
+  for book in books:
+    books_list = books_list + '<li>%s</li>' % book
+  books_list = books_list + '</ul>'
+
+  form = '''<form action="/books" method="POST">
+    <input type="text" name="title" />
+    <br />
+    <input type="submit" value="add book" />
+  </form>'''
+  return books_list + form
+
+
+@route('/books/<book_id:int>')
+def get_item(book_id):
+  """this page should show information about a single book"""
+  if book_id < len(books):
+    return "You requested the page for the book: %s" % books[book_id]
+  abort(404)
+
+
+@route('/books', method='POST')
+def add_item():
+  book_title = request.forms.get('title')
+  books.append(book_title)
+  redirect('/books')
+
+run(host='localhost', port=8080)
